9ee55d83b43040a58bb2858acf410bbad12f7040
Hamza Israr <hamzaisrar12@gmail.com>
2024-07-26 14:44:14 +0500

    chore: Add Javadoc for Configuration

--- profile/src/main/java/org/openedx/profile/domain/model/Configuration.kt
+++ profile/src/main/java/org/openedx/profile/domain/model/Configuration.kt
@@ -6,6 +6,7 @@ import org.openedx.core.domain.model.AgreementUrls
  * @param isIAPEnabled In App Purchase is enabled or not
  * @param agreementUrls User agreement urls
  * @param faqUrl FAQ url
+ * @param feedbackFormUrl URL of the learner feedback form
  * @param supportEmail Email address of support
  * @param versionName Version of the application (1.0.0)
  */

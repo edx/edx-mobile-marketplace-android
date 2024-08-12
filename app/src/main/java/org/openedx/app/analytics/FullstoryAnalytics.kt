b3c6176a45797f64e6578f310e6f57c080d0cdcf
Farhan Arshad <farshad@2u.com>
2024-08-12 18:35:54 +0500

    feat: Crashlytics Integration with FullStory
    - Add a Fullstory session replay link to a crash report.
    
    fixes: LEARNER-10158

--- app/src/main/java/org/openedx/app/analytics/FullstoryAnalytics.kt
+++ app/src/main/java/org/openedx/app/analytics/FullstoryAnalytics.kt
@@ -2,9 +2,11 @@ package org.openedx.app.analytics
 
 import com.fullstory.FS
 import com.fullstory.FSSessionData
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import org.openedx.core.utils.Logger
 
-class FullstoryAnalytics : Analytics {
+
+class FullstoryAnalytics(isFirebaseEnabled: Boolean = false) : Analytics {
 
     private val logger = Logger(TAG)
 
@@ -12,6 +14,10 @@ class FullstoryAnalytics : Analytics {
         FS.setReadyListener { sessionData: FSSessionData ->
             val sessionUrl = sessionData.currentSessionURL
             logger.d { "FullStory Session URL is: $sessionUrl" }
+            if (isFirebaseEnabled) {
+                val instance = FirebaseCrashlytics.getInstance()
+                instance.setCustomKey("FSsessionURL", sessionUrl)
+            }
         }
     }
 

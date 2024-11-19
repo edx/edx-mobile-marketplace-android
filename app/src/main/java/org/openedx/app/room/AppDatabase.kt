fea66bb2a66debf45b9fb39aec78f0fa4bcd90ed
omer.habib <omer.habib26@gmail.com>
2024-11-19 17:07:20 +0500

    feat: Added offline support for CourseEnrollmentDetails API
    - Store API response in DB for offline mode
    
    fix: LEARNER-10282

--- app/src/main/java/org/openedx/app/room/AppDatabase.kt
+++ app/src/main/java/org/openedx/app/room/AppDatabase.kt
@@ -3,6 +3,9 @@ package org.openedx.app.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+import org.openedx.core.data.model.room.CourseEnrollmentDetailsEntity
 import org.openedx.core.data.model.room.CourseStructureEntity
 import org.openedx.core.data.model.room.discovery.EnrolledCourseEntity
 import org.openedx.core.module.db.DownloadDao
@@ -22,7 +25,8 @@ const val DATABASE_NAME = "OpenEdX_db"
         CourseEntity::class,
         EnrolledCourseEntity::class,
         CourseStructureEntity::class,
-        DownloadModelEntity::class
+        DownloadModelEntity::class,
+        CourseEnrollmentDetailsEntity::class,
     ],
     version = DATABASE_VERSION,
     exportSchema = false
@@ -33,4 +37,55 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun courseDao(): CourseDao
     abstract fun dashboardDao(): DashboardDao
     abstract fun downloadDao(): DownloadDao
+}
+
+val MIGRATION_1_2 = object : Migration(1, 2) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL("DROP TABLE IF EXISTS course_enrollment_details_table")
+        // Create the new table
+        database.execSQL(
+            """
+            CREATE TABLE IF NOT EXISTS course_enrollment_details_table (
+                id TEXT NOT NULL PRIMARY KEY,
+                courseUpdates TEXT NOT NULL,
+                courseHandouts TEXT NOT NULL,
+                discussionUrl TEXT NOT NULL,
+                hasUnmetPrerequisites INTEGER NOT NULL,
+                isTooEarly INTEGER NOT NULL,
+                isStaff INTEGER NOT NULL,
+                auditAccessExpires TEXT,
+                hasAccess INTEGER NOT NULL,
+                errorCode TEXT,
+                developerMessage TEXT,
+                userMessage TEXT,
+                additionalContextUserMessage TEXT,
+                userFragment TEXT,
+                certificateURL TEXT,
+                created TEXT,
+                mode TEXT,
+                isActive INTEGER NOT NULL,
+                upgradeDeadline TEXT,
+                name TEXT NOT NULL,
+                number TEXT NOT NULL,
+                org TEXT NOT NULL,
+                start TEXT,
+                startDisplay TEXT NOT NULL,
+                startType TEXT NOT NULL,
+                end TEXT,
+                isSelfPaced INTEGER NOT NULL,
+                bannerImage_uri TEXT,
+                bannerImage_uriAbsolute TEXT,
+                courseImage_uri TEXT,
+                courseImage_name TEXT,
+                courseVideo_uri TEXT,
+                image_large TEXT,
+                image_small TEXT,
+                image_raw TEXT,
+                facebook TEXT NOT NULL,
+                twitter TEXT NOT NULL,
+                courseAbout TEXT NOT NULL
+            )
+        """.trimIndent()
+        )
+    }
 }

8148936898ef8beaae15d615008ee1616ece6b2d
Omer Habib <30689349+omerhabib26@users.noreply.github.com>
2025-01-16 22:06:22 +0500

    chore: Implement video stream priority for native videos (#62)
    chore: Implement video stream priority for native videos
    
    - Handle video streaming priority based on user preference
    
    fix: LEARNER-10260

--- app/src/main/java/org/openedx/app/di/ScreenModule.kt
+++ app/src/main/java/org/openedx/app/di/ScreenModule.kt
@@ -316,6 +316,7 @@ val screenModule = module {
             get(),
             get(),
             get(),
+            get(),
         )
     }
     viewModel { (courseId: String, courseTitle: String) ->

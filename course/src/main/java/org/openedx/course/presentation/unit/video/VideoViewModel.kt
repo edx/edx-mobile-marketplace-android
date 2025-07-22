5bebd5c5bcc63e0aa26d47a97558c0cba96ba4f0
Farhan Arshad <43750646+farhan-arshad-dev@users.noreply.github.com>
2025-07-22 13:45:52 +0500

    fix: Captions Disappear After Rotating Device to Landscape Mode (#150)
    * fix: Captions Disappear After Rotating Device to Landscape Mode
    
    - Reapply the saved captions to the player upon orientation change.
    - Remove unused code from VideoViewModel.
    
    fixes: LEARNER-10499

--- course/src/main/java/org/openedx/course/presentation/unit/video/VideoViewModel.kt
+++ course/src/main/java/org/openedx/course/presentation/unit/video/VideoViewModel.kt
@@ -1,17 +1,12 @@
 package org.openedx.course.presentation.unit.video
 
-import android.net.Uri
 import androidx.lifecycle.viewModelScope
 import androidx.media3.common.C
-import androidx.media3.common.MediaItem
-import androidx.media3.common.MimeTypes
 import kotlinx.coroutines.launch
 import org.openedx.core.data.storage.CorePreferences
-import org.openedx.core.domain.model.VideoPlaybackSpeed
 import org.openedx.core.system.notifier.CourseCompletionSet
 import org.openedx.core.system.notifier.CourseNotifier
 import org.openedx.core.system.notifier.CourseVideoPositionChanged
-import org.openedx.core.utils.LocaleUtils
 import org.openedx.core.utils.Logger
 import org.openedx.course.data.repository.CourseRepository
 import org.openedx.course.presentation.CourseAnalytics
@@ -35,24 +30,6 @@ class VideoViewModel(
         get() = preferencesManager.videoSettings
 
     private var isBlockAlreadyCompleted = false
-
-    var transcripts = emptyMap<String, String>()
-    var selectedLanguage: String = ""
-    val subtitleConfigurations: List<MediaItem.SubtitleConfiguration>
-        get() = transcripts
-            .toSortedMap(
-                compareBy { LocaleUtils.getLanguageByLanguageCode(it) }
-            )
-            .map { (language, uri) ->
-                val selectionFlags =
-                    if (language == selectedLanguage) C.SELECTION_FLAG_DEFAULT else 0
-
-                MediaItem.SubtitleConfiguration.Builder(Uri.parse(uri))
-                    .setMimeType(MimeTypes.APPLICATION_SUBRIP)
-                    .setSelectionFlags(selectionFlags)
-                    .setLanguage(language)
-                    .build()
-            }
 
     fun sendTime() {
         if (currentVideoTime != C.TIME_UNSET) {
@@ -89,13 +66,6 @@ class VideoViewModel(
                 }
             }
         }
-    }
-
-    fun getVideoQuality() = videoSettings.videoStreamingQuality
-
-    fun setVideoPlaybackSpeed(speed: Float) {
-        preferencesManager.videoSettings =
-            videoSettings.copy(videoPlaybackSpeed = VideoPlaybackSpeed.getVideoPlaybackSpeed(speed))
     }
 
     companion object {

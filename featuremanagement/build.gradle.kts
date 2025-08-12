deb5d4a18f23aa1d197a9ee6fd50e52eec73fa93
Hamza Israr <hisrar@edx.org>
2025-08-12 06:58:00 +0500

    feat: Update NDK to 28+ for 16 KB page size support
    Fixes: LEARNER-10710

--- featuremanagement/build.gradle.kts
+++ featuremanagement/build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
 android {
     namespace = "org.openedx.featuremanagement"
     compileSdk = 34
+    ndkVersion = "28.2.13676358"
 
     defaultConfig {
         minSdk = 24
--- notifications/build.gradle.kts
+++ notifications/build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
 android {
     namespace = "org.openedx.notifications"
     compileSdk = 34
+    ndkVersion = "28.2.13676358"
 
     defaultConfig {
         minSdk = 24

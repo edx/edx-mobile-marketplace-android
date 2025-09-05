dfbfd98016cbd31872d6de40a12c667984d29fae
maheshchanthani <cmahesh-sonata@2u.com>
2025-09-05 11:54:14 +0530

    refactor: Add Loader on POST Actions in Discussions

--- discussion/src/main/java/org/openedx/discussion/presentation/responses/DiscussionResponsesFragment.kt
+++ discussion/src/main/java/org/openedx/discussion/presentation/responses/DiscussionResponsesFragment.kt
@@ -500,7 +500,7 @@ private fun DiscussionResponsesScreen(
                                                 }
                                             } else {
                                                 CircularProgressIndicator(
-                                                    modifier = Modifier.padding(7.dp),
+                                                    modifier = Modifier.size(48.dp).padding(7.dp),
                                                     color = MaterialTheme.appColors.primary,
                                                 )
                                             }

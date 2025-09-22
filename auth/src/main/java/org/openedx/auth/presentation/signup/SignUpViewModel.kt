8e3ce1bf3e9dd5f6eeaa7106937128e19954185d
sandeepkumar <dsandeepkumar-sonata@2u.com>
2025-09-22 16:29:24 +0530

    refactor: Removed captcha token boolean value

--- auth/src/main/java/org/openedx/auth/presentation/signup/SignUpViewModel.kt
+++ auth/src/main/java/org/openedx/auth/presentation/signup/SignUpViewModel.kt
@@ -162,8 +162,7 @@ class SignUpViewModel(
             }
         )
         val mapFields = uiState.value.allFields.associate { it.name to it.placeholder } +
-                mapOf(ApiConstants.RegistrationFields.HONOR_CODE to true.toString())+
-                mapOf(ApiConstants.RegistrationFields.CAPTCHA_TOKEN to true.toString())
+                mapOf(ApiConstants.RegistrationFields.HONOR_CODE to true.toString())
         val resultMap = mapFields.toMutableMap()
         if(captchaName.isNotNullOrEmpty()) {
             reCaptchaToken?.let { resultMap.put(captchaName.toString(), it) }

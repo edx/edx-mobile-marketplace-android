61e6b7ab0314592ab8f914040995429f829d5c77
maheshchathani <cmahesh-sonata@2u.com>
2026-01-09 15:36:44 +0530

    refactor: Few crashes reported in firebase for Android.....

--- core/src/main/java/org/openedx/core/presentation/IAPAnalytics.kt
+++ core/src/main/java/org/openedx/core/presentation/IAPAnalytics.kt
@@ -91,6 +91,5 @@ enum class IAPAnalyticsKeys(val key: String) {
     ERROR_ALERT_TYPE("error_alert_type"),
     SHOW_CERTIFICATE_PREVIEW("show_certificate_preview"),
     CERT_PREVIEW_VARIENT("cert_preview_varient"),
-    PRICE("price"),
     ATTEMPTS_TO_PURCHASE("attempts_to_purchase")
 }

5678c98d6f9c1e8939f0b0ff75842fdbef7a4a97
kalalmounesh <mkalal-sonata@2u.com>
2026-03-26 14:59:20 +0530

    fix: Revert "reformatted AuthRepository with recaptcha"
    This reverts commit 90ea650b8a7d5f664e533128ac363dc99f35ff18.

--- app/src/main/java/org/openedx/app/di/ScreenModule.kt
+++ app/src/main/java/org/openedx/app/di/ScreenModule.kt
@@ -92,8 +92,7 @@ val screenModule = module {
     }
     viewModel { MainViewModel(get(), get(), get(), get(), get()) }
 
-    factory { AuthRepository(get(), get(), get(),
-        get()) }
+    factory { AuthRepository(get(), get(), get(),get()) }
     factory { AuthInteractor(get()) }
     factory { Validator() }
 

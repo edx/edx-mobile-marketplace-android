a57768760bda3a2518a3cc6fa57651039e5835e2
dsandeepkumar2830 <dsandeepkumar-sonata@2u.com>
2026-05-05 15:57:08 +0530

    fix: Added Pip Entry and Exit mode

--- course/src/main/java/org/openedx/course/presentation/unit/video/PipViewModel.kt
+++ course/src/main/java/org/openedx/course/presentation/unit/video/PipViewModel.kt
@@ -1,12 +1,15 @@
 package org.openedx.course.presentation.unit.video
 
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 import org.openedx.core.BaseViewModel
 import org.openedx.course.data.repository.player.PlayerController
 import org.openedx.course.domain.interactor.PipInteractor
+import org.openedx.course.domain.interactor.model.PipAction
 import org.openedx.course.domain.interactor.model.PipPlayerState
 import org.openedx.course.domain.interactor.model.PipPlayerType
 
@@ -37,9 +40,23 @@ class PipViewModel(
         pipInteractor.updatePlaybackState(isPlaying, isEnded)
     }
 
+    fun enterPipMode() {
+        pipInteractor.enterPipMode()
+        viewModelScope.launch {
+            _pipEvent.emit(PipUiEvent.PipModeEntered)
+        }
+    }
+
+    fun exitPipMode() {
+        pipInteractor.exitPipMode()
+        viewModelScope.launch {
+            _pipEvent.emit(PipUiEvent.PipModeExited)
+        }
+    }
+
     // --- PiP Actions ---
 
-    fun handleAction(action: PißpAction) {
+    fun handleAction(action: PipAction) {
         pipInteractor.handleAction(action)
     }
 

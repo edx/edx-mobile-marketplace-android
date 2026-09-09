b573dba82f7172f5e9264df746f5b5a5d109d48e
dsandeepkumar2830 <dsandeepkumar-sonata@2u.com>
2026-09-09 14:28:53 +0530

    fix: added context import in ScreenModule.kt

--- app/src/main/java/org/openedx/app/di/ScreenModule.kt
+++ app/src/main/java/org/openedx/app/di/ScreenModule.kt
@@ -1,5 +1,6 @@
 package org.openedx.app.di
 
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
